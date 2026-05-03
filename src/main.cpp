3fe790da24a8893ce6ab8cc37efa3e604e5f9f29
km7dev <100500023+Kingminer7@users.noreply.github.com>
2026-05-02 21:09:20 -0400

    More buttons, setting for chance

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
+#include "Geode/cocos/cocoa/CCObject.h"
+#include "Geode/utils/string.hpp"
 #include <Geode/Geode.hpp>
+#include <Geode/ui/Button.hpp>
+#include <Geode/utils/VMTHookManager.hpp>
+#include <alphalaneous.alphas_geode_utils/include/ObjectModify.hpp>
 
 using namespace geode::prelude;
 
@@ -18,41 +23,88 @@ using namespace geode::prelude;
         }
     };
 
+template <string::ConstexprString S, typename T>
+T const& getSettingFast() {
+    static T value = (
+        listenForSettingChanges<T>(S.data(), [](T val) {
+            value = std::move(val);
+        }),
+        getMod()->getSettingValue<T>(S.data())
+    );
+    return value;
+}
 
-#include <Geode/modify/CCMenuItemSpriteExtra.hpp>
-class $modify(CCEryItem, CCMenuItemSpriteExtra) {
-	bool init(CCNode* a, CCNode* b, CCObject* rec, SEL_MenuHandler selector) {
-		if (!CCMenuItemSpriteExtra::init(a, b, rec, selector)) {
+bool rejectPress() {
+    if (random::generate<int>(0, 100) < getSettingFast<"chance", int>()) return false;
+    FMODAudioEngine::get()->playEffect("s14061.ogg"_spr);
+    std::string str = "";
+    for (int i = 0; i < random::generate(2, 4); i++) {
+        if (i > 0) str += "\n\n";
+        auto r = random::generate(0, reasons.size() + special.size() * 2);
+        if (r >= reasons.size()) {
+            r -= reasons.size();
+            str += special[r / 2]();
+        } else {
+            str += reasons[r];
+        }
+    }
+    MDPopup::create("Your press was rejected by Ery.", str, "Ok")->show();
+    return true;
+}
+
+void addErySprite(CCNode* node) {
+    queueInMainThread([item = Ref(node)](){
+        auto spr = CCSprite::create("eri.png"_spr);
+        spr->setScaleX(item->getContentWidth() / spr->getContentWidth());
+        spr->setScaleY(item->getContentHeight() / spr->getContentHeight());
+        item->addChildAtPosition(spr, Anchor::Center, {}, false);
+    });
+}
+
+#include <Geode/modify/CCMenuItem.hpp>
+class $modify(CCEryItem, CCMenuItem) {
+	bool initWithTarget(CCObject* rec, SEL_MenuHandler sel) {
+		if (!CCMenuItem::initWithTarget(rec, sel)) {
 			return false;
 		}
 
-        queueInMainThread([item = Ref(this)](){
-            auto spr = CCSprite::create("eri.png"_spr);
-            spr->setScaleX(item->getContentWidth() / spr->getContentWidth());
-            spr->setScaleY(item->getContentHeight() / spr->getContentHeight());
-            item->addChildAtPosition(spr, Anchor::Center, {}, false);
-        });
-
+        addErySprite(this);
 		return true;
 	}
 
     void activate() {
-        if (random::generate<int>(0,2)) {
-            CCMenuItemSpriteExtra::activate();
-        } else {
-            FMODAudioEngine::get()->playEffect("s14061.ogg"_spr);
-            std::string str = "";
-            for (int i = 0; i < random::generate(2, 4); i++) {
-                if (i > 0) str += "\n\n";
-                auto r = random::generate(0, reasons.size() + special.size() * 2);
-                if (r >= reasons.size()) {
-                    r -= reasons.size();
-                    str += special[r / 2]();
-                } else {
-                    str += reasons[r];
-                }
-            }
-            MDPopup::create("Your press was rejected by Ery.", str, "Ok")->show();
-        }
+        // ccmenuitemspriteextra breaks kinda when its not the orig method hooked
+        if (getUserFlag("mise")) return CCMenuItem::activate();
+        if (!rejectPress()) CCMenuItem::activate();
+    }
+};
+
+#include <Geode/modify/CCMenuItemSpriteExtra.hpp>
+class $modify(CCEryItemSpriteExtra, CCMenuItemSpriteExtra) {
+    bool init(CCNode* a, CCNode* b, CCObject* rec, SEL_MenuHandler sel) {
+        if (!CCMenuItemSpriteExtra::init(a,b,rec,sel)) return false;
+        setUserFlag("mise"_spr);
+        return true;
+    }
+
+    void activate() {
+        if (!rejectPress()) CCMenuItemSpriteExtra::activate();
+    }
+};
+
+void eryGeodeButton(Button* btn) {
+    if (!rejectPress()) btn->activate();
+}
+
+class $classModify(EryButton, geode::Button) {
+    void modify() {
+        if (Loader::get()->isPatchless()) return log::warn("No geode::Button support, jitless detected.");
+        addErySprite(this);
+
+        auto hook = VMTHookManager::get().addHook<ResolveC<EryButton>::func(&EryButton::activate)>(this, "geode::Button::activate");
+    }
+
+    void activate() override {
+        if (!rejectPress()) Button::activate();
     }
 };
